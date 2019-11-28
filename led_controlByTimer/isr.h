b49dd03a4ee4cb5e0181fe912b5170b1cd4f0937
v77777 <973415811@qq.com>
2019-11-28 14:39:23 +0800

    中断相关

---
+++ led_controlByTimer/isr.h
@@ -0,0 +1,18 @@
+#ifndef __isr_h
+#define __isr_h
+// Hal: exp: #define P_led P10 -----------------
+
+// Const: exp: #define D_data 1 ----------------
+
+// Globle Var -----------------------------------------
+#ifdef __isr_c
+bit b1ms;
+#else
+extern bit b1ms;
+#endif
+
+// Action Macro: exp: #define F_getData() ------
+
+// Function ------------------------------------
+
+#endif
