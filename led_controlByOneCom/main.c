af91b8b95c66fc6cba7d3d6abba9a97d216b3012
v77777 <973415811@qq.com>
2019-11-30 22:12:04 +0800

    借助计时器和中断技术实现单端口控制LED灯（存在些许灵敏度问题）

--- led_controlByOneCom/main.c
+++ led_controlByOneCom/main.c
@@ -1,7 +1,6 @@
 #define __main_c
 #include "includeAll.h"
 int modeValue = 1;
-void delayMs(uint16_t time);
 //=============================================================================
 void main() {
 
@@ -30,7 +29,7 @@ static uint8_t timer5ms = 0;
     timer5ms++;
   }
   if (timer5ms >= 5) {
-		P1MODL = 0xa8;
+		P1MODL = 0xa8;//将模式置为上拉输入,感知按键是否按下，只要处理时间够短，不影响LED显示
     	GetKeys();
   }
 }
@@ -40,20 +39,14 @@ void TaskProcess() {}
 void TaskSetting() {
 	 // delayMs(1000);
     if (modeValue == 1){//模式标志为1时，该模式为推挽输出
-		//获取按键信息
-	//	P1MODL = 0xa8;		//将模式置为上拉输入,感知按键是否按下，只要处理时间够短，不影响LED显示
-	//	GetKeys();
-	//	delayMs(50);		//此处延时为确保能成功获取按键
 		//LED状态转换
 		if(D_keyValue1 == keyValue){
 			modeValue = ~modeValue;		//模式标志为取反
 		}
 		P1MODL = 0xaa;		//将模式置为推挽输出，使LED显示
-		//delayMs(1000);
 		}
-	else {//否则，该模式为上拉输入
-			//P1MODL = 0xa8;	
-			//GetKeys();
+	else {
+	//否则，该模式为上拉输入
 			if(D_keyValue1 == keyValue){
 				modeValue = ~modeValue;	
 			}
@@ -61,13 +54,4 @@ void TaskSetting() {
 }
 //=============================================================================
 void DisplayProcess() {
-  // F_ledOn();
-}
-
-void delayMs(uint16_t time){
-	uint16_t i,j;
-	for(i=0; i<time; i++){
-		for(j=0; j<1000; j++){
-		}
-	}
 }
