ced85247ed79851bf0d64311c7392f0694250453
v77777 <973415811@qq.com>
2019-12-21 20:39:33 +0800

    项目所用头文件

---
+++ 68b_kettle_Control/code(78Bsop32)/includeAll.h
@@ -0,0 +1,11 @@
+#include <INTRINS.H>
+#include <REGtenxTM52F5268B.h>
+
+#include "typeAlias.h"
+#include "buzz.h"
+#include "display.h"
+#include "triac.h"
+#include "init.h"
+#include "isr.h"
+#include "keys.h"
+#include "main.h"
