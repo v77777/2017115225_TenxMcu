e8842b40e55d8a8d6b86d6127ce7c556c29cc9e3
v77777 <973415811@qq.com>
2020-01-03 16:33:33 +0800

    按键测试 K1永为0,原因未知 K4开启，K2温度+10,K3温度-10；

--- 68b_kettle_Control/code(78Bsop32)/keys.c
+++ 68b_kettle_Control/code(78Bsop32)/keys.c
@@ -2,7 +2,7 @@
 #include "includeAll.h"
 //=============================================================================
 void GetKeys() {
-	
+	int delay = 0;
   static uint8_t tempKeyValue = D_keyNull;
 
   if (P_key1 == 0||P_key2 == 0||P_key3 == 0||P_key4 == 0) {
@@ -14,12 +14,38 @@ void GetKeys() {
   }
 	
 	if(P_key4 == 0){
-		key4 = 0;
+	key4 = 0; 
+	//led12 = 1;
 	}
-	if(P_key2 == 0){
-		key3 = 0;
+	//
+	
+}
+
+void GetKey1(){
+	//P_key1 euqal 0
+	if(P_key1 == 0){key3 = 0; 
+	//led12 = 1;
 	}
-	if(P_key1 == 0){
-		i=i+10;
+	
+}
+
+void GetKey2(){
+	if(P_key2 == 0&& key_flag == 1){
+		if((i+10)<100){	i=i+10;}
+		else{i=99;}
+		key_flag = 0;
+		led12 = 1;
 	}
+	//
+}
+
+
+void GetKey3(){
+	if(P_key3 == 0 && key_flag == 1){
+		if((i-10)>0){ i = i - 10;}
+		else{ i = 0; }
+		key_flag = 0;
+		led12 = 1;
+	}
+	//led12 = 1;
 }
