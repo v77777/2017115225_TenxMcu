076a794e5420cd90e9d885ce24cedf6f3852e093
v77777 <973415811@qq.com>
2020-01-03 16:15:33 +0800

    开启及温度设置 错误：将 设置温度时的 while（1） 设为while（key3） 时，数置超过50就马上跳出循环

--- 68b_kettle_Control/code(78Bsop32)/main.c
+++ 68b_kettle_Control/code(78Bsop32)/main.c
@@ -9,12 +9,15 @@
 #define __main_c
 #include "includeAll.h"
 int modeValue = 1;
-int i = 32;
 int j = 10;
+int k = 0;
 int flag = 0;
+
 //=============================================================================
 void main() {
-	int k;
+
+	
+	i = 25;
   SysInit();
   VarsInit();
   InitTriac();
@@ -26,7 +29,23 @@ void main() {
 	P1_4 = 0;
 	P1_2 = 0;
 	P1MODH = 0xaa;     //推挽输出状态控制蜂鸣器
+	key4 = 1;
+	key3 = 1;
   while (1) {
+  	while(key4){
+  		F_clearWDT();
+			TimeProcess();
+  		DisplayOn(1);
+			//DisplayNumber(i);		
+		}
+		
+		while(1){
+  		F_clearWDT();
+		  TimeProcess();
+  		DisplayNumber(i);		
+
+			//TaskSetting();
+		}
     //喂狗
     F_clearWDT();
     TimeProcess();
@@ -37,21 +56,24 @@ void main() {
 //=============================================================================
 void TimeProcess() {
   static uint8_t timer5ms = 0;
+  static uint8_t timer100ms = 0;
   static uint16_t second = 0;
-	uint16_t temp1,temp2,temp3,temp4;
-	
+  uint16_t temp1,temp2,temp3,temp4,temp7,temp6,temp5,temp8;
+  	
 	
   if (b1ms) {
     // 1ms 执行一次
     b1ms = 0;
     timer5ms++;
+		timer100ms++;
     second++;
 }
   if (timer5ms >= D_5ms) {
-/*	temp1 = P1MODL;
-	temp2 = P1MODH;
-	temp3 = P3MODL;
-	temp4 = P3MODH;
+	timer5ms = 0;
+	temp5 = P1MODL;
+	temp6 = P1MODH;
+	temp7 = P3MODL;
+	temp8 = P3MODH;
 	
 	//1.7
 	P1MODH = 0x00;
@@ -62,43 +84,106 @@ void TimeProcess() {
 	//1.2
 	P1MODL = 0x00;
 	//SysInit();
-	//P3_4????
+	//P3_4
+	temp1 = P3_4;
+	temp2 = P3_2;
+	temp3 = P1_7;
+	temp4 = P1_2;
 	P3_4 = 1;
 	P3_2 = 1;
-	//P1_7????*/
+	//P1_7????
 	P1_2 = 1;
 	P1_7 = 1;
-	P1MODL = P1MODL&0xCF;
   GetKeys();
-	P1_2 = 0;
-	P1_7 = 0;
+	P1_2 = temp4;
+	P1_7 = temp3;
+	P3_4 = temp1;
+	P3_2 = temp2;
 
-	/*P3_4 = 0;
-	P3_2 = 0;
-	P1MODL = temp1;
-	P1MODH = temp2;
-	P3MODL = temp3;
-	P3MODH = temp4;*/
-
-
+	P1MODL = temp5;
+	P1MODH = temp6;
+	P3MODL = temp7;
+	P3MODH = temp8;
   }
-  if (second >= D_1000ms) {
-      second = 0;
-			GetTemp();
+	
+	if (timer100ms >= 100) {
+		timer100ms = 0;
+		temp5 = P1MODL;
+	temp6 = P1MODH;
+	temp7 = P3MODL;
+	temp8 = P3MODH;
+	P1MODH = 0x00;
+	P3MODL = 0x00;
+	P3MODH = 0x00;
+	P1MODL = 0x00;
+	temp1 = P3_4;
+	temp2 = P3_2;
+	temp3 = P1_7;
+	temp4 = P1_2;
+	P3_4 = 1;
+	P3_2 = 1;
+	P1_2 = 1;
+	P1_7 = 1;
+		GetKey1();
+	P1_2 = temp4;
+	P1_7 = temp3;
+	P3_4 = temp1;
+	P3_2 = temp2;
+
+	P1MODL = temp5;
+	P1MODH = temp6;
+	P3MODL = temp7;
+	P3MODH = temp8;
+		GetTemp();
+	}
+	
+if (second >= D_200ms) {
+	temp5 = P1MODL;
+	temp6 = P1MODH;
+	temp7 = P3MODL;
+	temp8 = P3MODH;
+	P1MODH = 0x00;
+	P3MODL = 0x00;
+	P3MODH = 0x00;
+	P1MODL = 0x00;
+	temp1 = P3_4;
+	temp2 = P3_2;
+	temp3 = P1_7;
+	temp4 = P1_2;
+	P3_4 = 1;
+	P3_2 = 1;
+	P1_2 = 1;
+	P1_7 = 1;
+    second = 0;
+		GetKey2();
+	P3_4 = 1;
+	P3_2 = 1;
+	P1_2 = 1;
+	P1_7 = 1;
+		GetKey3();
+	P1_2 = temp4;
+	P1_7 = temp3;
+	P3_4 = temp1;
+	P3_2 = temp2;
+
+	P1MODL = temp5;
+	P1MODH = temp6;
+	P3MODL = temp7;
+	P3MODH = temp8;
+		key_flag = 1;
   }
 }
 //=============================================================================
 void TaskProcess() {}
 //=============================================================================
 void TaskSetting() {
-    if (modeValue == 1){//模式标志为1时，该模式为推挽输出
-		//LED状态转换
+    if (modeValue == 1){
 		if(D_keyValue1 == keyValue){
 			modeValue = ~modeValue;		//模式标志为取反
 			led12 = ~led12;
 			P1_0 = ~P1_0;
-			i = i+1;
-			DisplayProcess();
+			i = i+10;
+			//DisplayProcess();
 		}
 		//P1MODL = P1MODL|0x20;		//将模式置为推挽输出，使LED显示
 		
@@ -129,3 +214,4 @@ void DisplayProcess() {
 	P1MODH = 0xaa;     //推挽输出状态控制蜂鸣器
 	//DisplayClear();
 }
+
--- 68b_kettle_Control/code(78Bsop32)/main.h
+++ 68b_kettle_Control/code(78Bsop32)/main.h
@@ -5,20 +5,27 @@
 // Const: exp: #define D_data 1 ----------------
 // base 1ms
 #define D_5ms 5
-#define D_1000ms 3000
+#define D_1000ms 1000
+#define D_200ms 200
 // Globle Var -----------------------------------------
 #ifdef __main_c
 bit bLedFlash;
 bit led12;
 bit led14;
+bit key4;
+bit key3;
 bit b4ms;
 uint8_t tick;
+uint8_t i;
 #else
 extern bit b4ms;
 extern bit led12;
 extern bit led14;
+extern bit key4;
+extern bit key3;
 extern bit bLedFlash;
 extern uint8_t tick;
+extern uint8_t i;
 #endif
 
 // Action Macro: exp: #define F_getData() ------
